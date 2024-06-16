2bdbb40a706840e456f71b2986f8b58c1824d5ce
Andrey Kazannikov <andrey.kazannikov@mail.ru>
2024-06-16 17:31:01 +0300

    fix del task problem: add id for task to delete tasks

--- src/api/routes/twilio_webhook.py
+++ src/api/routes/twilio_webhook.py
@@ -1,5 +1,4 @@
 import re
-import logging
 from fastapi import APIRouter, Request
 from twilio.twiml.messaging_response import MessagingResponse
 from src.utils.reminder_service import schedule_reminder, get_reminders, delete_reminder
@@ -7,48 +6,36 @@ from src.utils.twilio_client import send_twilio_message
 
 router = APIRouter()
 
-logger = logging.getLogger(__name__)
-
 
 @router.post("/webhooks/twilio")
 async def twilio_webhook(request: Request):
-    form_data = await request.form()
-    from_number = form_data.get("From")
-    body = form_data.get("Body")
+    form = await request.form()
+    from_number = form['From']
+    body = form['Body'].strip().lower()
 
-    reminder_regex = re.compile(r"remind me to (.+) at (\d{4}-\d{2}-\d{2} \d{2}:\d{2})(?: in (.+))?")
-    list_regex = re.compile(r"list reminders", re.IGNORECASE)
-    delete_regex = re.compile(r"delete reminder at (\d{4}-\d{2}-\d{2} \d{2}:\d{2})", re.IGNORECASE)
+    reminder_pattern = re.compile(r"remind me to (.+) at (\d{4}-\d{2}-\d{2} \d{2}:\d{2})(?: in (.+))?")
+    delete_pattern = re.compile(r"delete reminder (.+)")
+    list_pattern = re.compile(r"list reminders")
 
-    reminder_match = reminder_regex.match(body)
-    list_match = list_regex.match(body)
-    delete_match = delete_regex.match(body)
-
-    if reminder_match:
+    if reminder_match := reminder_pattern.match(body):
         reminder_text = reminder_match.group(1)
         reminder_time_str = reminder_match.group(2)
-        user_tz = reminder_match.group(3) or 'Europe/Moscow'
-
-        schedule_reminder(from_number, reminder_text, reminder_time_str, user_tz)
-        response_message = f"Reminder set for '{reminder_text}' at {reminder_time_str} in {user_tz}."
-        send_twilio_message(response_message, from_number)
-    elif list_match:
-        reminders = get_reminders(from_number)
-        if reminders:
-            response_message = "Here are your reminders:\n" + "\n".join(reminders)
+        user_tz = reminder_match.group(3) if reminder_match.group(3) else 'Europe/Moscow'
+        reminder_id = schedule_reminder(from_number, reminder_text, reminder_time_str, user_tz)
+        response_message = f"Reminder set for '{reminder_text}' at {reminder_time_str} in {user_tz}. [ID: {reminder_id}]"
+    elif delete_match := delete_pattern.match(body):
+        reminder_id = delete_match.group(1)
+        if delete_reminder(from_number, reminder_id):
+            response_message = f"Deleted reminder with ID {reminder_id}."
         else:
-            response_message = "You have no reminders."
-        send_twilio_message(response_message, from_number)
-    elif delete_match:
-        reminder_time_str = delete_match.group(1)
-        delete_reminder(from_number, reminder_time_str)
-        response_message = f"Reminder at {reminder_time_str} has been deleted."
-        send_twilio_message(response_message, from_number)
+            response_message = f"No reminder found with ID {reminder_id}."
+    elif list_pattern.match(body):
+        reminders = get_reminders(from_number)
+        response_message = "Here are your reminders:\n" + "\n".join(
+            reminders) if reminders else "You have no current reminders."
     else:
         response_message = (
-            "Sorry, I didn't understand that command. You can ask me to 'remind me to [task] at [YYYY-MM-DD HH:MM] in [Timezone]', "
-            "'list reminders' or 'delete reminder at [YYYY-MM-DD HH:MM]'."
-        )
-        send_twilio_message(response_message, from_number)
+            "Sorry, I didn't understand that command. You can ask me to 'remind me to [task] at [YYYY-MM-DD HH:MM] in [Timezone]' or 'list reminders' or 'delete reminder [ID]'.")
 
-    return MessagingResponse()
+    send_twilio_message(response_message, from_number)
+    return str(MessagingResponse())
--- src/utils/reminder_service.py
+++ src/utils/reminder_service.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime, timezone
 import pytz
 from src.utils.redis_client import redis_client
+import uuid
 
 logging.basicConfig(
     level=logging.INFO,
@@ -15,11 +16,12 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def save_reminder(user: str, reminder: str, reminder_time: datetime, user_tz: str):
-    reminder_id = f"reminder:{user}:{reminder_time.timestamp()}"
-    redis_client.hset(reminder_id,
-                      mapping={"reminder": reminder, "time": reminder_time.isoformat(), "user_tz": user_tz})
-    redis_client.zadd("reminders", {reminder_id: reminder_time.timestamp()})
+def save_reminder(user: str, reminder: str, reminder_time: datetime, user_tz: str, reminder_id: str):
+    reminder_key = f"reminder:{user}:{reminder_id}"
+    redis_client.hset(reminder_key,
+                      mapping={"reminder": reminder, "time": reminder_time.isoformat(), "user_tz": user_tz,
+                               "id": reminder_id})
+    redis_client.zadd("reminders", {reminder_key: reminder_time.timestamp()})
     logger.info(f"Saved reminder: {reminder} for user: {user} at time: {reminder_time} in timezone: {user_tz}")
 
 
@@ -44,16 +46,17 @@ def get_reminders(user: str):
             user_timezone = pytz.timezone(reminder['user_tz'])
             reminder_time_local = reminder_time_utc.astimezone(user_timezone)
             utc_offset = reminder_time_local.utcoffset().total_seconds() / 3600
-            if utc_offset != 0:
-                offset_sign = '+' if utc_offset >= 0 else '-'
-                offset_hours = int(abs(utc_offset))
-                offset_minutes = int((abs(utc_offset) * 60) % 60)
-                offset_str = f"UTC {offset_sign}{offset_hours:02}:{offset_minutes:02}"
-                reminder_texts.append(
-                    f"{reminder['reminder']} at {reminder_time_local.strftime('%Y-%m-%d %H:%M')} ({reminder['user_tz']}, {offset_str})")
-            else:
-                reminder_texts.append(
-                    f"{reminder['reminder']} at {reminder_time_local.strftime('%Y-%m-%d %H:%M')} ({reminder['user_tz']})")
+            offset_sign = '+' if utc_offset >= 0 else '-'
+            offset_hours = int(abs(utc_offset))
+            offset_minutes = int((abs(utc_offset) * 60) % 60)
+            offset_str = f"UTC {offset_sign}{offset_hours:02}:{offset_minutes:02}"
+            reminder_text = (
+                f"{reminder['reminder']} at {reminder_time_local.strftime('%Y-%m-%d %H:%M')} "
+                f"({reminder['user_tz']}, {offset_str})"
+            )
+            if 'id' in reminder:
+                reminder_text += f" [ID: {reminder['id']}]"
+            reminder_texts.append(reminder_text)
         else:
             logger.warning(f"Missing keys in reminder: {reminder}")
 
@@ -65,24 +68,23 @@ def schedule_reminder(user: str, reminder: str, reminder_time_str: str, user_tz:
     user_timezone = pytz.timezone(user_tz)
     reminder_time_local = datetime.strptime(reminder_time_str, '%Y-%m-%d %H:%M')
     reminder_time_utc = user_timezone.localize(reminder_time_local).astimezone(timezone.utc)
-    save_reminder(user, reminder, reminder_time_utc, user_tz)
+    reminder_id = str(uuid.uuid4())
+    save_reminder(user, reminder, reminder_time_utc, user_tz, reminder_id)
     delay = (reminder_time_utc - datetime.now(timezone.utc)).total_seconds()
     from src.utils.celery_client import send_reminder
-    send_reminder.apply_async((user, f"Reminder: {reminder}"), countdown=delay)
+    send_reminder.apply_async((user, f"Reminder: {reminder}"), countdown=delay, task_id=reminder_id)
     logger.info(
         f"Scheduled reminder: {reminder} for user: {user} at time: {reminder_time_utc} with delay: {delay} seconds")
+    return reminder_id
 
 
-def delete_reminder(user: str, reminder_time_str: str):
-    try:
-        reminder_time_local = datetime.strptime(reminder_time_str, '%Y-%m-%d %H:%M')
-        reminder_time_utc = reminder_time_local.astimezone(timezone.utc)
-        reminder_id = f"reminder:{user}:{reminder_time_utc.timestamp()}"
-
-        logger.info(f"Trying to delete reminder: {reminder_id}")
-
-        redis_client.delete(reminder_id)
-        redis_client.zrem("reminders", reminder_id)
-        logger.info(f"Deleted reminder for user: {user} at time: {reminder_time_utc}")
-    except Exception as e:
-        logger.error(f"Error deleting reminder: {e}")
+def delete_reminder(user: str, reminder_id: str):
+    reminder_keys = redis_client.keys(f"reminder:{user}:{reminder_id}")
+    if not reminder_keys:
+        logger.warning(f"No reminders found for user: {user} with ID: {reminder_id}")
+        return False
+    for reminder_key in reminder_keys:
+        redis_client.delete(reminder_key)
+        redis_client.zrem("reminders", reminder_key)
+        logger.info(f"Deleted reminder for user: {user} with ID: {reminder_id}")
+    return True
